47e16ffc058a5230f09dab1763740e187d357850
ViktorChernichko <victor845679a@gmail.com>
2025-12-25 18:17:03 +0200

    Add files via upload

---
+++ DZ6/DZ6/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DZ6.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DZ6.Data;
+
+public class ApplicationDbContext : IdentityDbContext<MyIdentityUser>
+{
+    public DbSet<TagModel> Tags { get; set; }
+    public DbSet<BookModel> Books { get; set; }
+    
+    public DbSet<PostModel> Posts { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ DZ6/DZ6/Models/MyIdentityUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DZ6.Models;
+
+public class MyIdentityUser : IdentityUser
+{
+    public ICollection<PostModel> Posts { get; set; } = new List<PostModel>();
+}
